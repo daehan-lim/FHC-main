3e7696d4b176fe69475adf7d79160837b0a2a411
Daehan Lim <penjan.eng@gmail.com>
2023-09-06 18:48:30 +0900

    First commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# Federated Hypersphere Classifier (FHC)
+
+This repository provides implementations for ***Anomaly Detection in Multi-host Environment Based on Federated Hypersphere Classifier***, In progress.
---
+++ src/optimizer/__init__.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# @Created   : 2022/02/13 3:21 PM
+# @Author    : Junhyung Kwon
+# @Site      : 
+# @File      : __init__.py.py
+# @Software  : PyCharm
+
+
+from .fhctrainer_v2 import RHSCTrainerV2
